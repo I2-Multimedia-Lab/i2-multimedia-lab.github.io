7818710a02493f362a66df7675ba02d2b27bf963
Kang You <33834531+youkangGG@users.noreply.github.com>
2023-01-27 10:35:38 +0800

    Add files via upload

---
+++ contents/news.js
@@ -0,0 +1,45 @@
+// {
+//     'title':'',     // 标题
+//     'date':'',      // 年月日
+//     'newbrand':true,    // 是否显示新闻前的小红色(New)标志
+//     'content':'',   // 用户点开新闻之后的内容。该部分如果要分段的话可以这么写：'content':'<p>第一段</p><p>第二段</p><p>第三段</p>'
+// }
+
+news = [
+    {
+        'title':'New homepage of our I2ML is published online!',
+        'date':'2023-01-25',
+        'newbrand':true,
+        'content':'Publishing content from a website can be accomplished simply by manipulating dictionary objects! See our GitHub for details on the source code and how to use it!😄',
+    },
+    {
+        'title':'Our paper accepted by ACM Multimedia APCCPA Workshop 2022!',
+        'date':'2022-08-08',
+        'newbrand':false,
+        'content':'<p>Congratulations on Youkang!</p><p>Congratulations on Youkang\'s research on point cloud compression, which was accepted as Oral by the workshop APCCPA of ACM MM 2022, a CCF class A conference in the multimedia field.</p><p>ABSTRACT: Point cloud is a crucial representation of 3D contents, which has been widely used in many areas such as virtual reality, mixed reality, autonomous driving, etc. With the boost of the number of points in the data, how to efficiently compress point cloud becomes a challenging problem. In this paper, we propose a set of significant improvements to patch-based point cloud compression, i.e., a learnable context model for entropy coding, octree coding for sampling centroid points, and an integrated compression and training process. In addition, we propose an adversarial network to improve the uniformity of points during reconstruction. Our experiments show that the improved patch-based autoencoder outperforms the state-of-the-art in terms of rate-distortion performance, on both sparse and large-scale point clouds. More importantly, our method can maintain a short compression time while ensuring the reconstruction quality.</p>',
+    },
+    {
+        'title':'Our paper accepted by IJCAI 2022!',
+        'date':'2022-04-21',
+        'newbrand':false,
+        'content':"Congratulations on Tian Haoyue's research results being hired by the International Joint Conference on Artificial Intelligence (IJCAI 2022), the top conference in category A of CCF, with only 15% hiring. The paper was also selected as Long Presentation (Long Show is another 25% of the accepted papers). Congratulations on Haoyue's breakthrough, which is very difficult. Very well-deserved achievement! Congratulations and Well Done!",
+    },
+    {
+        'title':'Our paper accepted by IEEE ICASSP 2022!',
+        'date':'2022-01-22',
+        'newbrand':false,
+        'content':"Congratulations on the acceptance of Hao Yue's research by the prestigious International Conference IEEE ICASSP 2022 in the field of image/video processing, which is scheduled to be held in Singapore in May. IEEE ICASSP is the type B CCF conference recommended by the Chinese Computer Society. Congratulations on Hao Yue, Well done!🌹🌹🌹",
+    },
+    {
+        'title':'Our paper of point cloud compression accepted by ACM Multimedia Asia 2021!',
+        'date':'2021-09-28',
+        'newbrand':false,
+        'content':"Congratulations on Youkang's research on three-dimensional point cloud compression and reconstruction, which was hired by ACM Multimedia Asia 2021, a prestigious international conference in the multimedia field. The hiring rate is only 32.7%. Congratulations! Well Done!",
+    },
+    {
+        'title':'Our paper of accepted by ICCV2021 workshop on Adversarial Robustness In the Real World!',
+        'date':'2021-08-13',
+        'newbrand':false,
+        'content':"Congratulations on Cheng Cheng's research, which was received by workshop on Adversarial Robustness In the Real World of IEEE Int. Conf. Computer Vision (ICCV2021). This is a great result, as the paper has undergone several rounds of modifications and has been accepted at last. Congratulations to Zhang Cheng, Very well deserved!",
+    },
+]
